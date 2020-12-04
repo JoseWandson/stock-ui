d6273c4d3e7d3bd4996fe4e4b36a8e3c99115201
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-12-03 22:32:42 -0300

    Criando o componente Input

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
+import Input from '../../shared/Input';
 import Header from '../Header';
 import './App.css';
 
 function App() {
+  const [street, setStreet] = useState('');
+
   return (
     <div className="App">
       <Header title="Stock" />
@@ -13,6 +16,7 @@ function App() {
         <Button onClick={() => window.alert('UIIIU')}>
           Alert
         </Button>
+        <Input label="Street" placeholder="E.g.: 15h Avenue" value={street} onChange={e => setStreet(e.target.value)} />
       </Container>
     </div>
   );
