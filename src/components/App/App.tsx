77c9e1dbaadb229daf6a39e8d911a79b84eac139
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-12-05 16:50:51 -0300

    Preparando para criar o componente de tabela

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,15 +9,7 @@ function App() {
       <Header title="Stock" />
 
       <Container>
-        <ul>
-          {
-            ['Daniel', 'Willian', 'Thiago', 'Daniel'].map((name, index) => {
-              return <li key={index}>
-                {name}
-              </li>
-            })
-          }
-        </ul>
+
       </Container>
     </div>
   );
---
+++ src/shared/Table/Table.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Table.scss';
+
+const Table = () => <table></table>;
+
+export default Table;
