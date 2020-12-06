2ce153f184fbcbcbd96b65352fc5e837ea588a86
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-12-05 21:10:28 -0300

    Estrutura HTML de uma tabela

--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
-import './Table.scss';
+// import './Table.scss';
 
-const Table = () => <table></table>;
+const Table = () =>
+    <table>
+        <thead>
+            <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Stock</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Coockie</td>
+                <td>$1.25</td>
+                <td>23</td>
+            </tr>
+            <tr>
+                <td>Milk 1L</td>
+                <td>$0.99</td>
+                <td>10</td>
+            </tr>
+        </tbody>
+    </table>
 
 export default Table;
