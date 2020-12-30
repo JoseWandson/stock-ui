67f74ce3167e46c92d914a44e363dcb7aa4521e2
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2020-12-30 18:30:22 -0300

    Adição do axios ao projeto

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import http from './utils/http';
+
+http.get('/posts');
 
 ReactDOM.render(
   <React.StrictMode>
