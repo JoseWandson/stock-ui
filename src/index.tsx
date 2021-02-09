395a91672dd4fc7569d2bcdeea091985c87adcbd
José Wandson Cavalcante Siqueira <wandsonacop@hotmail.com>
2021-02-08 21:30:46 -0300

    COnfigurando o redux no projeto

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import http from './utils/http';
-
-http.get('/posts');
+import { Provider } from 'react-redux';
+import store from './redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
